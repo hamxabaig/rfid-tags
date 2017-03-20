702b0d3e1feaa172f72b91e66e3628cf063589bd
mrrk181 <raafayrashid@gmail.com>
2017-03-21 03:10:04 +0500

    Made a form for viewingPersonel Currently only delete Button workds but does not delte

--- client/src/containers/pages/displayPersonnel.js
+++ client/src/containers/pages/displayPersonnel.js
@@ -14,7 +14,6 @@ export default class displayPeronnel extends Component{
   };
 
     componentDidMount(){
-        alert("did Mount personnel");
         superagent.get('/api/persons').end((err,res) => {
             if(!err){
               this.setState({persons: res.body});
@@ -26,12 +25,58 @@ export default class displayPeronnel extends Component{
 
     }
     render(){
-      console.log(this.state.persons);
+      //console.log(this.state.persons);
         return(
-            <div>
-                <h1>Success</h1>
-                </div>
+
+                // <ul>{
+                //     this.state.persons.map((person)=>{
+                //         console.log("Does it come here??")
+                //         return(
+                //         <li> <strong> {person.army_number}</strong> : {person.name} </li>)
+                //     })
+                // }
+                // </ul>
+                <table className="table">
+                    <thead>
+                        <tr >
+                            <th>Army Number</th>
+                            <th>Name</th>
+                            <th>Rank</th>
+                            <th>Current Unit</th>
+                        </tr>
+                        </thead>
+
+                        {
+                            this.state.persons.map((person)=>{
+                                return(
+                                    <tr key={person._id}>
+                                        <th scope="row">{person.army_number}</th>
+                                        <td>{person.name}</td>
+                                        <td>{person.rank}</td>
+                                        <td>{person.current_unit}</td>
+                                        <td><button bsStyle='success' onClick={()=>this.deletePerson(person._id)}>Delete Person</button></td>
+                                        <td><button bsStyle='success' onClick={()=>this.editPerson(person._id)}>Edit</button></td>
+                                        <td><button bsStyle='success' onClick={()=>this.viewPerson(person._id)}>View Person</button></td>
+
+                                    </tr>
+
+                                )
+                            })
+                        }
+                </table>
 );
+
+    }
+    deletePerson(id){
+        superagent.delete('/api/persons').send({_id:id}).end((err,res)=>{
+            if(!err){
+                console.log("deleted");
+            }
+            else{
+                console.log("error occured");
+            }
+        });
+        console.log("Clicked"+id);
 
     }
     getPersons (){
