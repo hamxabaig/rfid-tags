af54d77419fd67aeb155a012ed973805a60f9536
mrrk181 <raafayrashid@gmail.com>
2017-05-14 23:22:35 +0500

    changed Readme

--- client/src/containers/Layout/index.js
+++ client/src/containers/Layout/index.js
@@ -11,6 +11,8 @@ import NotificationSystem from 'react-notification-system';
 import { Link, browserHistory } from 'react-router'
 import Radium from 'radium'
 import { signoutUser } from '../../actions/user.js'
+import { user } from '../../server/models/controllers/user.js'
+
 const menuItem = [
     {
         key : 1,
@@ -226,7 +228,7 @@ class Layout extends React.Component {
 
     }
     componentDidMount() {
-        
+
     }
 
 
